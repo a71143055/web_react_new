f94384568921d945e0a4e2a9776cbc51d1097357
a71143055 <a71143055@gmail.com>
2024-11-21 15:47:30 +0900

    web_react_new

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import NameForm from "./chp11/NameForm";
+import NameRequestForm from "./chp11/NameRequestForm";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NameForm />
+    <NameRequestForm />
   </React.StrictMode>
 );
 
