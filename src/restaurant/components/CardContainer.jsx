0d2c72e74173aa75bdb5520a27869cd329c74e15
a71143055 <a71143055@gmail.com>
2024-12-06 17:04:07 +0900

    web_react_new

--- src/restaurant/components/CardContainer.jsx
+++ src/restaurant/components/CardContainer.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import Card from './Card'
-import img from '../imgs/img.png'
-import img1 from '../imgs/img_1.png'
+import img1 from '../imgs/img1.png'
 import img2 from '../imgs/img_2.png'
+import img3 from '../imgs/img_3.png'
 
 const listing = [
     {
         id : 1,
-        title : "서울의 아늑한 아파트",
+        title : "서울의 유명한 레스토랑",
         price : 100000,
-        imageUrl: `${img}`
-    },
-    {
-        id : 2,
-        title : "제주도 바다 뷰 펜션",
-        price : 150000,
         imageUrl: `${img1}`
     },
     {
-        id : 3,
-        title : "부산 해운대 근처 호텔",
-        price : 200000,
+        id : 2,
+        title : "제주도 바다 뷰 레스토랑",
+        price : 150000,
         imageUrl: `${img2}`
+    },
+    {
+        id : 3,
+        title : "부산 해운대 근처 호텔 레스토랑",
+        price : 200000,
+        imageUrl: `${img3}`
     },
 ];
 
