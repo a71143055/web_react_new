1a2f013ffb9b930dda3d4ab8fd97c06668e16441
a71143055 <a71143055@gmail.com>
2024-12-06 17:11:59 +0900

    web_react_new

--- src/restaurant/components/Hero.jsx
+++ src/restaurant/components/Hero.jsx
@@ -1,11 +1,11 @@
 import Reat from 'react';
-import bgImage from '../imgs/img_3.png'
+import bgImage from '../imgs/img.png'
 
 function Hero() {
     return(
         <div className="hero" style={{backgroundImage : `url(${bgImage}`}}>
-            <h1>특별한 숙소를 예약하세요</h1>
-            <p>에어비앤비에서 독특한 숙소를 찾아보세요.</p>
+            <h1>특별한 레스토랑을 예약하세요</h1>
+            <p>다이닝테이블닷컴에서 독특한 레스토랑을 찾아보세요.</p>
         </div>
     );
 }
